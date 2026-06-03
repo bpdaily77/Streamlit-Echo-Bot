52c7c7a5de371b2d334b8fbe26c3b50ae93919cf
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:41:11 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -10,9 +10,13 @@ if "messages" not in st.session_state:
 
 
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    if message["role"] == "user":
+        name, avatar = "Human", "🧑"
+    else:
+        name, avatar = "Skynet", "🤖"
+        
+    with st.chat_message(message["role"], avatar=avatar, name=name):
         st.markdown(message["content"])
 
 # React to user input
