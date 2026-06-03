3188874b55a88aced12ad6c33fb13f6102bda962
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:20:21 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -3,6 +3,6 @@ import numpy as np
 
 with st.chat_message("assistant", avatar="🤖"):
     # Displays the name in a small, muted font right inside the bubble
-    st.caption("**Cyberdyne Systems Model 101**") 
+    st.caption("**Skynet**") 
     st.write("Are you John Connor?")
     st.bar_chart(np.random.randn(30, 3))
