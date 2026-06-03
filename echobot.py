fae1a06f0db5a6a875e599fe03f7e377a48a8cdd
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:21:25 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -6,3 +6,6 @@ with st.chat_message("assistant", avatar="🤖"):
     st.caption("**Skynet**") 
     st.write("Are you John Connor?")
     st.bar_chart(np.random.randn(30, 3))
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
