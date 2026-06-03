6bff815ab80f76585664537e98a43633beecb4d3
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:34:52 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -15,19 +15,34 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 
 
-
-with st.chat_message("assistant", avatar="🤖"):
+# with st.chat_message("assistant", avatar="🤖"):
     # Displays the name in a small, muted font right inside the bubble
-    st.caption("**Skynet**") 
-    st.write("Are you John Connor?")
+    # st.caption("**Skynet**") 
+    # st.write("Are you John Connor?")
 
-prompt = st.chat_input("Say something")
-if prompt:
-    with st.chat_message("user", avatar="🧑"):
+# prompt = st.chat_input("Say something")
+# if prompt:
+    # with st.chat_message("user", avatar="🧑"):
         # Displays the name in a small, muted font right inside the bubble
-        st.caption("**Human**") 
-        st.write(prompt)
+        # st.caption("**Human**") 
+        # st.write(prompt)
