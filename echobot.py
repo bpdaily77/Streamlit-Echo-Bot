b630742b750c1b396c632027bde601363372e3d2
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:21:53 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -5,7 +5,7 @@ with st.chat_message("assistant", avatar="🤖"):
     # Displays the name in a small, muted font right inside the bubble
     st.caption("**Skynet**") 
     st.write("Are you John Connor?")
-    st.bar_chart(np.random.randn(30, 3))
+
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
