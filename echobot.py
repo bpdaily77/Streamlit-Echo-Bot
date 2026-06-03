123b8b5d0d2222f2e44a0e03a541c6eb9523b15c
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:31:07 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,6 +1,25 @@
 import streamlit as st
 import numpy as np
 
+st.title("Echo Bot")
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+
+
+
+
 with st.chat_message("assistant", avatar="🤖"):
     # Displays the name in a small, muted font right inside the bubble
     st.caption("**Skynet**") 
