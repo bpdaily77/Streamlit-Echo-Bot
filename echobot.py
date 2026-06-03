413923ee5a626ea555682350852627082a643e33
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:39:47 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -36,15 +36,3 @@ if prompt := st.chat_input("What is up?"):
 
 
 
-
-# with st.chat_message("assistant", avatar="🤖"):
-    # Displays the name in a small, muted font right inside the bubble
-    # st.caption("**Skynet**") 
-    # st.write("Are you John Connor?")
-
-# prompt = st.chat_input("Say something")
-# if prompt:
-    # with st.chat_message("user", avatar="🧑"):
-        # Displays the name in a small, muted font right inside the bubble
-        # st.caption("**Human**") 
-        # st.write(prompt)
