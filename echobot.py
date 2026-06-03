bf2b7801f343b1eb83e5100634deafc2ee5a76a9
bpdaily77 <bpdaily@gmail.com>
2026-06-03 14:16:59 -0400

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
 
-with st.chat_message("Cyberdyne Systems Model 101"):
+with st.chat_message("Cyberdyne Systems Model 101", avatar="🤖"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
